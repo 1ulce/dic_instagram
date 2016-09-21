7574c937f96594ad274cf300603ad2d28bc98a95
1ulce <shun.1ulce@gmail.com>
2016-09-21 21:41:52 +0900

    [update] ability not to show by un-auth user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,5 +28,9 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+    if user && user.admin?
+      can :access, :rails_admin   # grant access to rails_admin
+      can :manage, :all           # allow superadmins to do anything
+    end
   end
 end
